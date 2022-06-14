8a7ad0b799e59562bfe6a95dfb84f6fa2b43dca9
Mr-Dan <35195104+Mr-Dan@users.noreply.github.com>
2022-06-14 10:35:22 +0300

    Update 2-1.py

--- 2/2-1.py
+++ 2/2-1.py
@@ -2,13 +2,13 @@
 #а) Поменять местами самую длинную и самую короткую строки в списке
 #б) Отсортировать список, чтобы строки шли по алфавиту
 list_ = ["cat", "dog", "hamster", "duck", "goose", "rabbit", "sheep"]
-max_=list_.index(max(list_, key=len))
-min_=list_.index(min(list_, key=len))
+max_=list_.index(max(list_, key=len)) #поменяю
+min_=list_.index(min(list_, key=len)) #поменяю
 list_[min_], list_[max_] = list_[max_], list_[min_]
 
 for i in range(len(list_)):
     print(list_[i])
 print()
-list_.sort()
+list_.sort() #поменяю
 for i in range(len(list_)):
     print(list_[i])
